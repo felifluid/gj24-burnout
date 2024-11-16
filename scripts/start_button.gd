4aef6484b39e6b9d0104282fe92743fc42cfc861
mewo2000 <60525434+mewo2000@users.noreply.github.com>
2024-11-16 22:47:05 +0100

    feat (main_menu): prototyp main menu. new game and continue switch to clicker scene. exit button exits

---
+++ scripts/start_button.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+func _on_pressed() -> void:
+	print("1")
+	get_tree().change_scene_to_file("res://scenes/clicker.tscn")
+	print("2")
