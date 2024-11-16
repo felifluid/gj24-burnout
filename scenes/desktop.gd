be5adf65a0736c4cbe0a23554eabbca4dccac5a9
felifluid <coding@felifluid.de>
2024-11-16 19:14:41 +0100

    fix (interrupt): change string to float property

--- scenes/desktop.gd
+++ scenes/desktop.gd
@@ -4,6 +4,9 @@ extends Control
 @onready var clock : Label = get_node("Clock")
 
 @export var dayHours : int
+@export var workshift : int
+
+signal clock_changed(time : float)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,9 +17,10 @@ func _process(delta: float) -> void:
 	updateClock()
 	pass
 
+
 func getTime() -> float:
 	var ellapsed_time : float  = dayTimer.wait_time - dayTimer.time_left
-	return (ellapsed_time / dayTimer.wait_time) * dayHours + 8 # 10 hour work day, starting at 8 am
+	return (ellapsed_time / dayTimer.wait_time) * dayHours + workshift # 10 hour work day, starting at 8 am
 
 func updateClock() -> void:
 	var time : float = getTime()
@@ -26,5 +30,6 @@ func updateClock() -> void:
 	if (minutes < 10):
 		zero = "0"
 	clock.text = str(hours) + ":" + str(zero) + str(minutes)
-	if (dayTimer.wait_time - dayTimer.time_left > dayTimer.wait_time / dayHours * 8):
+	if (dayTimer.wait_time - dayTimer.time_left > dayTimer.wait_time / dayHours * workshift):
 		clock.add_theme_color_override("font_color", Color.RED)
+	clock_changed.emit(time)
--- scripts/popup.gd
+++ scripts/popup.gd
@@ -1,19 +1,20 @@
 extends Window
 
-@onready var popup: Window = get_node(".")
-@export var pause: float = 5.0
+@export var showtime : float
 
 # Called when the node enters the scene tree for the first time.
 func _init():
 	self.hide()
-
-func _ready():
-	appear()
 	
 func appear():
-	get_tree().create_timer(10.0).timeout
+	#get_tree().create_timer(10.0).timeout
 	self.show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_control_clock_changed(time: float) -> void:
+	if (time >= showtime) :
+		appear()
