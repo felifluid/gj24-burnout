06f0a3c10fd894613bbdbdc696f52fc5cef8f284
felifluid <coding@felifluid.de>
2024-11-16 17:50:28 +0100

    feat (clicker): add cookie fadeout

--- scripts/cookie_clicker.gd
+++ scripts/cookie_clicker.gd
@@ -9,23 +9,18 @@ var points : int = 0
 @onready var range_min : Vector2 = Vector2(0,0) + Vector2(10,10)
 @onready var range_max : Vector2 = size - Vector2(90, 90)
 
-func increase_points() -> void:
-	points += 1
-	
-func update_label_text() -> void:
+func increase_points(value : int) -> void:
+	points += value
 	label.text = "Punkte: " + str(points)
 
 func spawn_cookie() -> void:
 	var cookie_instance = cookie.instantiate()
 	add_child(cookie_instance)
-	cookie_instance.connect("pressed", self._on_button_pressed)
 	cookie_instance.set_position(Vector2(randf_range(range_min.x, range_max.x), randf_range(range_min.y, range_max.y)))
-	
+	cookie_instance.connect("buttonDone", self._on_button_done)
 					
-func _on_button_pressed() -> void:
-	increase_points()
-	update_label_text()
-
+func _on_button_done(value : int) -> void:
+	increase_points(value)
 
 func _on_timer_timeout() -> void:
 	spawn_cookie()
