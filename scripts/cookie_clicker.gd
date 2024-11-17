ede351318878848bab306c66a9d61b5af4f2a7b5
felifluid <coding@felifluid.de>
2024-11-17 15:58:02 +0100

    feat (clicker): ADD COMBO WUUUUH

--- scripts/cookie_clicker.gd
+++ scripts/cookie_clicker.gd
@@ -2,15 +2,23 @@ class_name CookieClicker
 extends Control
 
 @export var label : Label
+@export var labelCombo : Label
+@export var comboDeclineCD : Timer
+@export var comboDecline : Timer
+
 var cookie = preload("res://assets/cookie.tscn")
 
 var points : int = 0
+var combo : int = 1
 
 @onready var range_min : Vector2 = Vector2(0,0) + Vector2(10,10)
 @onready var range_max : Vector2 = size - Vector2(90, 90)
 
+func _ready() -> void:
+	update_combo_label()
+
 func increase_points(value : int) -> void:
-	points += value
+	points += value * combo
 	label.text = "Punkte: " + str(points)
 
 func spawn_cookie() -> void:
@@ -21,6 +29,27 @@ func spawn_cookie() -> void:
 					
 func _on_button_done(value : int) -> void:
 	increase_points(value)
+	increase_combo()
+	comboDecline.paused = true
+	comboDeclineCD.start() # reset timer
+
+func increase_combo() -> void:
+	combo = combo * 2
+	update_combo_label()
+
+func decrease_combo() -> void:
+	if (combo > 1):
+		combo = combo / 2
+		update_combo_label()
+
+func update_combo_label() -> void:
+	labelCombo.text = "COMBO x" + str(combo)
 
 func _on_timer_timeout() -> void:
 	spawn_cookie()
+
+func _on_combo_decline_timeout() -> void:
+	decrease_combo()
+
+func _on_combo_decline_cooldown_timeout() -> void:
+	comboDecline.paused = false
